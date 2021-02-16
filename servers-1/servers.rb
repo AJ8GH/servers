faad4de60b9303e9e37b1ad62d81edce2f5ef118
Adam Jonas <adamjonas25@gmail.com>
2021-02-16 20:23:46 +0000

    solve all exercises in server 1

--- servers-1/servers.rb
+++ servers-1/servers.rb
@@ -1,23 +1,61 @@
 require 'socket'
 
+Exercise 1 -keep the server open
+server = TCPServer.new(2345)
+
+loop do
+  socket = server.accept
+  socket.puts 'What do you say?'
+  they_said = socket.gets.chomp
+  socket.puts "You said: #{they_said}. Goodbye! Standing by..."
+  socket.close
+end
+
+Exercise 2 - user can keep typing until they say quit
+
 server = TCPServer.new(2345)
 socket = server.accept
 
 loop do
-  socket.puts "what do you say?"
+  socket.puts 'What do you say?'
   they_said = socket.gets.chomp
-  exit if they_said == 'quit'
+  socket.close if they_said == 'quit'
 
-  socket.puts "You said: #{they_said}. Goodbye!"
+  socket.puts "You said #{they_said}. Goodbye! For now..."
 end
 
-# socket.close
+# Exercise 4 - interact with Notes app over the server
 
-server = TCPServer.new(2345)
-socket = server.accept
+class NoteList
+  def add_note(text)
+    notes << text
+  end
 
-socket.puts "what do you say?"
-they_said = socket.gets.chomp
-exit if they_said == 'quit'
-socket.
-socket.puts "You said: #{they_said}. Goodbye!"
+  def see_notes
+    notes.dup
+  end
+
+  private
+
+  attr_reader :notes
+
+  def initialize
+    @notes = []
+  end
+end
+
+note_list = NoteList.new
+
+loop do
+  socket.puts "would you like to 'add' a note, 'see' your notes, or 'quit'"
+  input = socket.gets.chomp
+  if input == 'add'
+    note = socket.gets.chomp
+    note_list.add_note(note)
+  elsif input == 'see'
+    socket.puts(note_list.see_notes)
+  elsif input == 'quit'
+    socket.close
+    exit
+  end
+end
